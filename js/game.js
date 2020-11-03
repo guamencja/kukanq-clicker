8a63f3285fa325cb4ba2d51491d53898026a6228
liskqu <56258709+lisqu16@users.noreply.github.com>
2020-11-03 19:25:17 +0100

    dzień, noc

--- js/game.js
+++ js/game.js
@@ -1,3 +1,32 @@
+let stan = false;
+
 // muzyka
-$(".gameWindow").innerHTML += "<audio loop id=\"day_sound\"><source src=\"./sound/day.mp3\" type=\"audio/mpeg\"></audio>";
-$("#day_sound").play();
+$(".gameWindow").innerHTML += "<audio loop id=\"day_sound\"><source src=\"./sound/day.mp3\" type=\"audio/mpeg\"></audio>\n<audio loop id=\"night_sound\"><source src=\"./sound/night.mp3\" type=\"audio/mpeg\"></audio>";
+
+// dzień, noc
+day();
+
+function night() { // funkcja, uruchamiająca noc
+    $("#day_sound").pause();
+    $("#day_sound").currentTime = 0;
+    $("#night_sound").play();
+    $(".gameWindow").style.background = "url(\"./img/night.jpg\")";  
+    stan = true;
+}
+
+function day() { // funkcja uruchamiająca dzień
+    $("#night_sound").pause();
+    $("#night_sound").currentTime = 0;
+    $("#day_sound").play();
+    $(".gameWindow").style.background = "url(\"./img/day.jpg\")";
+    stan = false;
+}
+
+// zmiana, co 8 minut
+setInterval(function(){
+    if (stan) 
+        day();
+    else 
+        night();
+}, 480000);
+
