2b6a08085a239bc3bbeb615a8964f394a8809e19
guam <lisek16@protonmail.com>
2022-09-08 11:20:32 +0200

    counter

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ const $ = (h) => {
 }
 
 // game functionality
+let chestnuts = 0;
+
 function isDay() { // based on real time
     // to do: get the hour from the server, a user can manipulate with an hour
     if (new Date().getHours() < 19) return true;
@@ -11,7 +13,23 @@ function isDay() { // based on real time
 }
 
 function load() {
+    chestnuts = parseInt(localStorage.chestnuts) | 0;
+    $("#counter").innerHTML = chestnuts;
     $("#game").style.backgroundImage = `url("./img/${isDay() ? "day" : "night"}.jpg")`;
+    
+    // saving
+    setInterval(() => {
+        localStorage.setItem("chestnuts", chestnuts);
+    }, 60000)
+}
+// when clicked
+$("#chestnut_tree").onclick = () => {
+    $("#counter").innerHTML = ++chestnuts;
 }
 
 load();
+
+// saving when leaving
+window.onbeforeunload = () => {
+    localStorage.setItem("chestnuts", chestnuts);
+}
